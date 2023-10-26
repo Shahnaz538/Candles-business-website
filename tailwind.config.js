b7bcac5f0a9b3190dd8d7a3fcac1e32cc4722c72
Shahnaz538 <131960221+Shahnaz538@users.noreply.github.com>
2023-10-26 18:48:41 +0500

    Half files of web added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    screens:{
+      xs:'320px',
+     sm:'480px',
+     md:'768px',
+     lg:'976px',
+     xl:'1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
